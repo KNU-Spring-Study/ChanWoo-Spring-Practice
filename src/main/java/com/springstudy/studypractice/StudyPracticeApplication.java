a3435a6a91f52848f755d171544c40750ab7a19d
woopaca <jcw001031@gmail.com>
2023-01-31 18:44:24 +0900

    initial commit

---
+++ src/main/java/com/springstudy/studypractice/StudyPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.studypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springstudy/studypractice/StudyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.studypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
