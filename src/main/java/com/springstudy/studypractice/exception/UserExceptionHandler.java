79a4b32f74ff299ac1bf30562ee9bfb6ce8d6001
woopaca <jcw001031@gmail.com>
2023-02-02 10:24:34 +0900

    Refactor: 예외처리 중복 제거 + HTTP Message Parsing Error 예외처리

--- src/main/java/com/springstudy/studypractice/exception/UserExceptionHandler.java
+++ src/main/java/com/springstudy/studypractice/exception/UserExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,33 +18,41 @@ public class UserExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(UserAuthException.class)
-    protected ResponseEntity<ErrorResponseDto> userAuthExceptionHandler(UserAuthException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> userAuthExceptionHandler(
+            UserAuthException e, HttpServletRequest request) {
         log.error("Message = {}", e.getUserValidError().getMessage());
 
-        String requestURI = request.getRequestURI();
-        HttpStatus httpStatus = e.getUserValidError().getHttpStatus();
-
-        return ResponseEntity.status(httpStatus)
-                .body(ErrorResponseDto.builder()
-                        .status(httpStatus.value())
-                        .errorType(httpStatus.name())
-                        .message(e.getUserValidError().getMessage())
-                        .path(requestURI)
-                        .build());
+        return createResponseEntity(
+                e.getUserValidError().getHttpStatus(), e.getUserValidError().getMessage(), request
+        );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponseDto> fieldValidationExceptionHandler(MethodArgumentNotValidException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> fieldValidationExceptionHandler(
+            MethodArgumentNotValidException e, HttpServletRequest request) {
         log.error("Message = {}", e.getFieldError().getDefaultMessage());
 
-        String requestURI = request.getRequestURI();
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        return createResponseEntity(HttpStatus.BAD_REQUEST, e.getFieldError().getDefaultMessage(), request);
+    }
 
-        return ResponseEntity.status(httpStatus)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorResponseDto> jsonParseExceptionHandler(HttpServletRequest request) {
+        String message = "HTTP 바디 메시지를 읽을 수 없습니다.";
+
+        log.error("Message = {}", message);
+
+        return createResponseEntity(HttpStatus.BAD_REQUEST, message, request);
+    }
+
+    private ResponseEntity<ErrorResponseDto> createResponseEntity(
+            HttpStatus status, String message, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+
+        return ResponseEntity.status(status)
                 .body(ErrorResponseDto.builder()
-                        .status(httpStatus.value())
-                        .errorType(httpStatus.name())
-                        .message(e.getFieldError().getDefaultMessage())
+                        .status(status.value())
+                        .errorType(status.name())
+                        .message(message)
                         .path(requestURI)
                         .build());
     }
