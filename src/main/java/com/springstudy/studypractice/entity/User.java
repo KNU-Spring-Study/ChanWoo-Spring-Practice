e20d0a5492feda23cb0ee75841e1cf9216b66840
woopaca <jcw001031@gmail.com>
2023-02-03 22:08:08 +0900

    UserDetails 구현

--- src/main/java/com/springstudy/studypractice/entity/User.java
+++ src/main/java/com/springstudy/studypractice/entity/User.java
@@ -1,16 +1,22 @@
 package com.springstudy.studypractice.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.springstudy.studypractice.controller.dto.SignUpRequestDto;
 import com.springstudy.studypractice.controller.dto.UserInfoResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,6 +25,9 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 // lombok을 사용하면 반복되는 코드를 줄일 수 있고, 필드를 추가할 때 getter, setter, 생성자를 수정하지 않아도 되어 편리한 기능이다.
@@ -27,7 +36,7 @@ import javax.validation.constraints.Pattern;
 @Builder // builder 패턴 (lombok)
 @NoArgsConstructor // 기본 생성자 (lombok)
 @AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자 (lombok)
-public class User {
+public class User implements UserDetails {
 
     @Id // Primary Key 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 값 생성 전략 -> MySQL에서 AUTO_INCREMENT인 경우 보통 IDENTITY
@@ -39,6 +48,7 @@ public class User {
     private String username;
 
     @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Email // Spring Validation - Email 형식 검증
@@ -54,6 +64,9 @@ public class User {
     @Enumerated(value = EnumType.STRING) // 열거형(Enum)의 이름을 String 타입으로 저장
     @Builder.Default // builder 패턴을 사용할 때 해당 필드의 값을 지정하지 않으면 null이 아닌 아래의 값이 default
     private Membership membership = Membership.BASIC;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
 
     // CollectionUserRepository 사용 시 필요
     public void setId(Long id) {
@@ -78,5 +91,34 @@ public class User {
                 .age(this.age)
                 .membership(membership)
                 .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isEnabled() {
+        return true;
     }
 }
